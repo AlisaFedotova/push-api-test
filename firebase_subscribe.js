8697a7f9cf70ead2a8018cfa66e20d528744f2fd
alicinia <alicinia97@gmail.com>
2018-12-28 16:02:05 +0700

    road notifications(2)

--- firebase_subscribe.js
+++ firebase_subscribe.js
@@ -101,3 +101,12 @@ function setTokenSentToServer(currentToken) {
         currentToken ? currentToken : ''
     );
 }
+
+// Handle incoming messages. Called when:
+// - a message is received while the app has focus
+// - the user clicks on an app notification created by a service worker
+//   `messaging.setBackgroundMessageHandler` handler.
+messaging.onMessage(function(payload) {
+    console.log('Message received. ', payload);
+    // ...
+});
